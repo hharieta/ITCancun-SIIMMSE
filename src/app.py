b39a096071798078e2e860c1e88c0d5a79a5506e
hharieta <L20530228@cancun.tecnm.mx>
2022-12-04 04:28:36 -0500

    Changed port env

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
 from flask import render_template, request
 import conf_server
+import os
 #from crypt import methods
+ON_HEROKU = os.environt.get('ON_HEROKU')
+
+if ON_HEROKU:
+    port = int(os.environ.get('PORT', 17995)) # as per OP comments default is 17995
+else:
+    port = 8000
 
 app = conf_server.connex_app
 
@@ -25,4 +32,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
