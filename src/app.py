a856162cfa7615792fd1486111c822615a145400
hharieta <L20530228@cancun.tecnm.mx>
2022-12-04 20:49:34 -0500

    __name__ __main__ deleted

--- src/app.py
+++ src/app.py
@@ -25,6 +25,6 @@ def form():
 def home():
     return render_template('home.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 8000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
